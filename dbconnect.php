709a488e0296567a1b456f60a2a1c33fd93032ae
hanaaprillia <hanaaprillia990@gmail.com>
2020-11-25 13:26:02 +0700

    first commit

---
+++ dbconnect.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$user = "id15414151_root";
+$password = "a<}g2QyDPkmX]m8H";
+$dbname = "id15414151_perpus";
+$conn = mysqli_connect($host, $user, $password, $dbname);
+
+if(!$conn){
+	die("error in connection");
+}
+
+//echo "database connected"
+?>
